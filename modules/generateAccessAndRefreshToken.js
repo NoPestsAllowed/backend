bb0f8f90999d372d82d7677377472bcd02d03169
Basile Favretto <bazil.f@gmail.com>
2024-05-22 14:18:42 +0200

    Increase token expiration date

--- modules/generateAccessAndRefreshToken.js
+++ modules/generateAccessAndRefreshToken.js
@@ -2,10 +2,10 @@ const jwt = require("jsonwebtoken");
 
 const generateAccessAndRefreshToken = (email) => {
     const accessToken = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "10s",
+        expiresIn: "10d",
     });
     const refreshToken = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1d",
+        expiresIn: "10d",
     });
 
     return [accessToken, refreshToken];
