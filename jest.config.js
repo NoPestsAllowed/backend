01a8215dd3ef573b7ad712171812f03aab3f73bf
Basile Favretto <bazil.f@gmail.com>
2024-06-14 09:39:57 +0200

    Testing for ci

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,9 @@
 const config = {
     verbose: true,
     testEnvironment: "node",
-    globalSetup: "./tests/utils/globalSetup.js",
-    globalTeardown: "./tests/utils/globalTeardown.js",
-    setupFilesAfterEnv: ["./tests/utils/setup.js"],
+    globalSetup: "../tests/utils/globalSetup.js",
+    globalTeardown: "../tests/utils/globalTeardown.js",
+    setupFilesAfterEnv: ["../tests/utils/setup.js"],
 };
 
 module.exports = config;
