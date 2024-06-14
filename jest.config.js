be70205d5fbfaf23b77fbf78bb104d847a66d360
Basile Favretto <bazil.f@gmail.com>
2024-06-14 10:18:25 +0200

    jest change

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,9 @@
 const config = {
     verbose: true,
     testEnvironment: "node",
-    globalSetup: "../__tests__/utils/globalSetup.js",
-    globalTeardown: "../__tests__/utils/globalTeardown.js",
-    setupFilesAfterEnv: ["<rootDir>/__tests__/utils/setup.js"],
+    globalSetup: "./__tests__/utils/globalSetup.js",
+    globalTeardown: "./__tests__/utils/globalTeardown.js",
+    setupFilesAfterEnv: ["./__tests__/utils/setup.js"],
 };
 
 module.exports = config;
