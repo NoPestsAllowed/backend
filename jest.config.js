f1272d66b87a0667b4901fe72e9e2c2eefe6460b
Basile Favretto <bazil.f@gmail.com>
2024-06-14 10:38:45 +0200

    jest change

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ const config = {
     verbose: true,
     testEnvironment: "node",
     rootDir: process.cwd(),
+    roots: ["<rootDir>, <rootDir>/../, <rootDir>/backend"],
     globalSetup: "<rootDir>/__tests__/utils/globalSetup.js",
     globalTeardown: "<rootDir>/__tests__/utils/globalTeardown.js",
     setupFilesAfterEnv: ["<rootDir>/__tests__/utils/setup.js"],
