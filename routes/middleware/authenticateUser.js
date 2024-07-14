0e5d0d5eeccec7c911ab207984f0e7bbba0f96b7
Basile Favretto <bazil.f@gmail.com>
2024-07-14 12:41:44 +0200

    Added nodemon & preparation to make server openid provider

--- routes/middleware/authenticateUser.js
+++ routes/middleware/authenticateUser.js
@@ -1,22 +1,77 @@
 const jwt = require("jsonwebtoken");
+const { promisify } = require("node:util");
+const jwksClient = require("jwks-rsa");
+const User = require("../../models/users");
 
 const authenticateUser = (req, res, next) => {
     const authHeader = req?.headers.authorization;
-    // console.log(authHeader);
     const token = authHeader && authHeader.split(" ")[1];
     if (!token) {
         return res.sendStatus(401);
     }
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) {
+    const fetchJwksUri = async (issuer) => {
+        const response = await fetch(`${issuer}/oidc/.well-known/openid-configuration`);
+        const { jwks_uri } = await response.json();
+        return jwks_uri;
+    };
+
+    const getKey = (jwksUri) => (header, callback) => {
+        console.log("jwksUri in getKey", jwksUri);
+        const client = jwksClient({ jwksUri });
+        client.getSigningKey(header.kid, (err, key) => {
+            if (err) {
+                return callback(err);
+            }
+            callback(null, key.publicKey || key.rsaPublicKey);
+        });
+    };
+
+    const verify = async (token) => {
+        const { iss: issuer } = jwt.decode(token);
+        console.log("issuer", issuer);
+        const jwksUri = await fetchJwksUri(issuer);
+        console.log("jwksUri", jwksUri);
+        return promisify(jwt.verify)(token, getKey(jwksUri));
+    };
+
+    const decodeToken = async (token) => {
+        const { iss: issuer } = jwt.decode(token);
+        console.log("issuer", issuer);
+        const jwksUri = await fetchJwksUri(issuer);
+        console.log("jwksUri", jwksUri);
+        return promisify(jwt.decode)(token, getKey(jwksUri));
+    };
+
+    const setUser = async (email) => {
+        const user = await User.findOne({ email: email });
+        req.user = user;
+        return user;
+    };
+    verify(token)
+        .then((user) => {
+            console.log("Token verified successfully.");
+            console.log(user);
+            // decodeToken(token).then((decodedToken) => console.log("decodedToken", decodedToken));
+            console.log("decoded token is ", jwt.decode(token, { complete: true }));
+            setUser(user.sub);
+            next();
+        })
+        .catch((err) => {
             console.log("auth error", err);
             return res.sendStatus(403);
-        }
+        });
 
-        req.user = user;
-        next();
-    });
+    // return res.sendStatus(403);
+    // jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, { algorithms: ["ES256"] }, (err, user) => {
+    //     if (err) {
+    //         console.log("auth error", err);
+    //         return res.sendStatus(403);
+    //     }
+
+    //     req.user = user;
+    //     next();
+    // });
 };
 
 module.exports = authenticateUser;
